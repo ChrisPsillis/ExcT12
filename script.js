67d64af1b29722eae09ffb59613de82a02ca36c6
ChrisPsillis <cpsi2013@gmail.com>
2022-02-24 14:14:38 +1100

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
+
+object.onclick = function(myFunction) {
+    myFunction: 'Dig'
+}
+
+console.log(onclick);

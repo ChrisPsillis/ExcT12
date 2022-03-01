c6d7bf11190c5a9a8d7c0794db94af4f35503ac9
ChrisPsillis <cpsi2013@gmail.com>
2022-03-01 19:53:13 +1100

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const h2 = document.createElement('h2');
 
 document.querySelector('body').appendChild(h2);
 
+let btn = document.createElement("button");
+btn.innerHTML = "Click Me";
+document.body.appendChild(btn);
